500874307b102c701257a318ac993c477521d93a
mikhail <sora@synzr.ru>
2023-03-22 21:42:10 +0500

    feat: flask-minify added

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from flask_minify import Minify
+
 from extensions import cache
 
 import blueprints
@@ -7,6 +9,7 @@ app = Flask(__name__)
 app.config.from_pyfile("configs/app.py")
 
 cache.init_app(app)
+Minify(app)
 
 app.register_blueprint(blueprints.index)
 app.register_blueprint(blueprints.api)
